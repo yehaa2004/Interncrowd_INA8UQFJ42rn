98a5653c6b6dc77e50e8cb9a5f2fc9804569d2be
Yehaasary kalimuthu <112149456+yehaa2004@users.noreply.github.com>
2023-07-14 15:40:55 +0530

    Add files via upload

---
+++ javascript- Task1.js
@@ -0,0 +1,9 @@
+javascript
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
